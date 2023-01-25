ada74915c0ed0c818fdbc08b4c0c93baa16915fd
Qicheng Geng <103839282+jackgeng19@users.noreply.github.com>
2023-01-25 14:40:16 -0500

    Update filter_real_time.py

--- filter_real_time.py
+++ filter_real_time.py
@@ -1,9 +1,29 @@
 import pyaudio
-import numpy
+import numpy as np
+from scipy.signal import notch_filter
 
+# Define the parameters of the filter
 sample_rate: int = 44011 #Hz
 quality: int = 30
+f_notch: int = 1000 #Hz
 
-f_notch: int = 100 #Hz
-
+# Open an input and output stream using PyAudio
 p = pyaudio.PyAudio()
+stream = p.open(format = pyaudio.paFloat32, channels = 1, rate = sample_rate, input = True, output = True)
+
+# Loop to apply the filter to the audio data in real-time
+while True:
+    # Read audio data from the input stream
+    data = stream.read(1024)
+    audio_data = np.fromstring(data, dtype=np.float32)
+
+    # Apply the notch filter to the audio data
+    filtered_data = notch_filter(audio_data, f_notch, quality, sample_rate)
+
+    # Write the filtered audio data to the output stream
+    stream.write(filtered_data.tostring())
+
+# Close the stream
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
